afa323650d11a8b880371728b5143fe338df334d
Sebastian A. Weiss <sebastian.weiss@nacamar.de>
2016-11-24 13:50:56 +0100

    cleanup

--- src/Example_Playout.php
+++ src/Example_Playout.php
@@ -1,18 +1,18 @@
 <?php
 
-header('Content-Type: text/plain');
-// error_reporting(E_ERROR | E_WARNING | E_PARSE);
+\header('Content-Type: text/plain');
+// \error_reporting(\E_ERROR | \E_WARNING | \E_PARSE);
 
 require_once '../vendor/autoload.php'; // HINT: path has to be adapted to your needs
 require_once './tools/AuthenticationHelper.inc.php';
 require_once './tools/Tools.inc.php';
 require_once './Credentials.inc.php';
 
-if (!\de\addvideo\examples\tools\AuthenticationHelper::authenticate(ACCOUNT, SECRET)) {
-    echo 'Unable to authenticate!', PHP_EOL;
+if (!\de\addvideo\examples\tools\AuthenticationHelper::authenticate(\ACCOUNT, \SECRET)) {
+    echo 'Unable to authenticate!', \PHP_EOL;
     exit();
 }
-echo 'Authenticated successfully!', PHP_EOL;
+echo 'Authenticated successfully!', \PHP_EOL;
 
 $entry_id = ""; // PUT a valid entry_id for a READY!!! entry here!
 // =============================================================================
@@ -26,7 +26,7 @@ try {
     $workflow_api_instance = new \de\addvideo\client\api\AddvideoworkflowApi();
     $playout_URLs = $workflow_api_instance->getPlayoutURLs($entry_id);
     if (isset($playout_URLs) && null !== $playout_URLs->getEntry()) {
-        echo "found!", PHP_EOL;
+        echo "found!", \PHP_EOL;
         /*
          * Reference to parent entry
          */
@@ -34,7 +34,7 @@ try {
         $playout_URLs_Set = $playout_URLs->getPlayoutUrlsSet();
         foreach ($playout_URLs_Set as $playout_URL) {
             $url = $playout_URL->getURL();
-            echo 'URL [URL: ' . $url . ', label: ' . $playout_URL->getLabel() . ', bitrate: ' . $playout_URL->getBitrate() . ', isProtected: ' . $playout_URL->getProtected() . '].', PHP_EOL;
+            echo 'URL [URL: ' . $url . ', label: ' . $playout_URL->getLabel() . ', bitrate: ' . $playout_URL->getBitrate() . ', isProtected: ' . $playout_URL->getProtected() . '].', \PHP_EOL;
             /*
              * Hint whether URL is protected against unauthorized access or not!
              */
@@ -42,7 +42,7 @@ try {
         }
     }
 } catch (\de\addvideo\client\ApiException $e) {
-    echo 'EXCEPTION: ', \de\addvideo\examples\tools\Tools::getExceptionString($e), PHP_EOL;
+    echo 'EXCEPTION: ', \de\addvideo\examples\tools\Tools::getExceptionString($e), \PHP_EOL;
 }
 echo "\n\n";
 
